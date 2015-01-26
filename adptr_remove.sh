d7b8972df01f1a66991059cde7b5299ab6a35016
wiw <anton.ivankin@gmail.com>
2015-01-26 16:09:04 +0600

    Fix error in perl script. Now it works! Command is prepaired to use.

--- adptr_remove.sh
+++ adptr_remove.sh
@@ -5,4 +5,4 @@ i=12
 adptr5=`echo $ADPTR_SHORT_5 | sed -e 's/^.\{'$i'\}//'` # Удаляю основания от адаптера с 5' конца
 adptr3=`echo $ADPTR_SHORT_3 | sed -e 's/.\{'$i'\}$//'` # Удаляю основания от адаптера с 3' конца
 
-cat $1 | perl -e 'while(<STDIN>) {$adptr5=$ARGV[0]; $adptr3=$ARGV[1]; $adptr5gatc=$adptr5.$ARGV[2]; $adptr3gatc=$adptr3.$ARGV[2]; $l=length($adptr5);  $id1=<STDIN>; $seq=<STDIN>; $id2=<STDIN>; $qual=<STDIN>; if($seq =~ /$adptr3gatc$/) {$seq=substr($seq, 1, -$l)."\n", length($seq); $qual=substr($qual,1, -$l) . "\n" }; if($seq =~ /^$adptr5gatc/){$seq=substr($seq,$l); $qual=substr($qual,$l)};  print $id1,$seq,$id2,$qual }' ${adptr5} ${adptr3} "GATC" > $2
+cat $1 | perl -e 'while(<STDIN>) { ($adptr5,$adptr3,$site)=@ARGV; $adptr5gatc=$adptr5.$site; $adptr3gatc=$site.$adptr3; $l=length($adptr5); $id1=$_; $seq=<STDIN>; $id2=<STDIN>; $qual=<STDIN>; if($seq =~ /$adptr3gatc$/) {$seq=substr($seq, 1, -$l-1)."\n"; $qual=substr($qual,1, -$l-1) . "\n" }; if($seq =~ /^$adptr5gatc/){$seq=substr($seq,$l); $qual=substr($qual,$l)}; print "$id1$seq$id2$qual" }' ${adptr5} ${adptr3} "GATC" > $2
